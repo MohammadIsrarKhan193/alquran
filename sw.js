44c6a4c2d2e74a8c8d3c3da25200887cba35eb3c
MohammadIsrarKhan193 <smartboy35th@gmail.com>
2026-01-04 00:18:17 +0430

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const cacheName = 'alquran-pro-v1';
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './logo.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
