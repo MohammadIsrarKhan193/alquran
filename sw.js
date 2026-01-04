c874cda3b6ed9802f2d42f699c5d97ef307cd959
MohammadIsrarKhan193 <smartboy35th@gmail.com>
2026-01-04 08:24:21 +0430

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,18 @@
-const cacheName = 'alquran-pro-v1';
+const cacheName = 'alquran-v1';
 const assets = [
   './',
   './index.html',
   './style.css',
   './script.js',
   './manifest.json',
-  './logo.png'
+  './logo.png',
+  './privacy.html'
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(assets);
-    })
-  );
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
